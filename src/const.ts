5be7a44e0fd9a057a76527126e9c6f4178eda6b2
霜序 <976060700@qq.com>
2025-02-22 20:44:37 +0800

    feat: 🎸 supprt fileType and sourceLocale in config.json (#5)
    ✅ Closes: #4

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,19 @@
+export enum LOCALE_FILE_TYPES {
+    TS = 'ts',
+    JS = 'js',
+    JSON = 'json',
+}
+
+export const SOURCE_LOCALE = 'zh-CN';
+
 export const DEFAULT_CONFIG_FILE = 'i18n.config.json';
 
 export const DEFAULT_CONFIG = {
-    localeDir: 'locales',
+    localeDir: './locales',
     extractDir: './',
     importStatement: 'import I18N from @/utils/i18n',
     excludeFile: [],
-    excludeDir: ['node_modules'],
-    type: 'ts',
+    excludeDir: ['node_modules', 'locales'],
+    type: LOCALE_FILE_TYPES.TS,
+    sourceLocale: SOURCE_LOCALE,
 };
--- src/init.ts
+++ src/init.ts
@@ -1,13 +1,30 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import { input, select } from '@inquirer/prompts';
 
-import { DEFAULT_CONFIG, DEFAULT_CONFIG_FILE } from './const';
+import {
+    DEFAULT_CONFIG,
+    DEFAULT_CONFIG_FILE,
+    LOCALE_FILE_TYPES,
+} from './const';
 import { success, error } from './utils';
 
-const createDefaultConfig = (localeDir: string, extractDir: string) => {
+interface InitOptions {
+    localeDir: string;
+    extractDir: string;
+    type: LOCALE_FILE_TYPES;
+    sourceLocale: string;
+}
+
+const createDefaultConfig = ({
+    localeDir,
+    extractDir,
+    sourceLocale,
+    type,
+}: InitOptions) => {
     const configFile = path.resolve(process.cwd(), `${DEFAULT_CONFIG_FILE}`);
     const config = JSON.stringify(
-        { ...DEFAULT_CONFIG, localeDir, extractDir },
+        { ...DEFAULT_CONFIG, localeDir, extractDir, sourceLocale, type },
         null,
         4,
     );
@@ -23,11 +40,15 @@ const createDefaultConfig = (localeDir: string, extractDir: string) => {
     }
 };
 
-const createLocaleFile = (localeDir: string) => {
-    const fileDir = `${localeDir}/zh-CN`;
+const createLocaleFile = ({
+    localeDir,
+    sourceLocale,
+    type,
+}: Omit<InitOptions, 'extractDir'>) => {
+    const fileDir = `${localeDir}/${sourceLocale}`;
     if (!fs.existsSync(fileDir)) {
         fs.mkdirSync(fileDir);
-        fs.writeFile(`${fileDir}/index.json`, '{}', (err) => {
+        fs.writeFile(`${fileDir}/index.${type}`, '{}', (err) => {
             if (err) {
                 error('创建国际化文件失败');
                 return;
@@ -37,14 +58,41 @@ const createLocaleFile = (localeDir: string) => {
     }
 };
 
-const init = (localeDir: string, extractDir: string) => {
+const init = async () => {
+    const localeDir = await input({
+        default: DEFAULT_CONFIG.localeDir,
+        message: '请输入国际化文件夹',
+    });
+    const extractDir = await input({
+        default: DEFAULT_CONFIG.extractDir,
+        message: '请输入提取中文的文件夹',
+    });
+    const sourceLocale = await input({
+        default: DEFAULT_CONFIG.sourceLocale,
+        message: '请输入源语言',
+    });
+    const type = await select({
+        message: '请选择语言文件类型',
+        default: LOCALE_FILE_TYPES.TS,
+        choices: [
+            { name: 'ts', value: LOCALE_FILE_TYPES.TS },
+            { name: 'js', value: LOCALE_FILE_TYPES.JS },
+            { name: 'json', value: LOCALE_FILE_TYPES.JSON },
+        ],
+    });
+
     const createLocaleDir = localeDir || DEFAULT_CONFIG.localeDir;
     const createExtractDir = extractDir || DEFAULT_CONFIG.extractDir;
     if (!fs.existsSync(createLocaleDir)) {
         fs.mkdirSync(createLocaleDir);
     }
-    createLocaleFile(createLocaleDir);
-    createDefaultConfig(createLocaleDir, createExtractDir);
+    createLocaleFile({ localeDir: createLocaleDir, sourceLocale, type });
+    createDefaultConfig({
+        localeDir: createLocaleDir,
+        extractDir: createExtractDir,
+        sourceLocale,
+        type,
+    });
 };
 
 export default init;
