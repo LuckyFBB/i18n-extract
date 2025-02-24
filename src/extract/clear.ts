9ba740d3d04d068c1e1aaec99e50808464d208d9
霜序 <976060700@qq.com>
2025-02-24 17:47:51 +0800

    feat: 🎸 supprt clear unused text in locale file (#3)
    * feat: 🎸 supprt clear unused text in locale file
    
    * feat: 🎸 sopprt clear onther lacale file
    
    * feat: 🎸 support clear data when use export default by identifier
    
    * feat: 🎸 remove I18N.get when clear
    
    ✅ Closes: #2

---
+++ src/extract/clear.ts
@@ -0,0 +1,143 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import _ from 'lodash';
+import { parse, ParserOptions } from '@babel/parser';
+import babelTraverse from '@babel/traverse';
+import * as babelTypes from '@babel/types';
+
+import {
+    generateLocaleKey,
+    getFilteredFiles,
+    getProjectConfig,
+    getSubDirectories,
+    parseLocaleModule,
+    success,
+    updateLocaleContent,
+} from '../utils';
+
+const {
+    importStatement,
+    type,
+    localeDir,
+    extractDir,
+    excludeDir,
+    excludeFile,
+} = getProjectConfig();
+
+const importVariable = importStatement
+    .replace(/^import\s+|\s+from\s+/g, ',')
+    .split(',')?.[1];
+
+const extractI18nFromScript = (
+    fileName: string,
+    extractMap: Record<string, any>,
+) => {
+    const sourceCode = fs.readFileSync(fileName, 'utf-8');
+    const plugins: ParserOptions['plugins'] = [
+        'decorators-legacy',
+        'typescript',
+    ];
+    if (fileName.endsWith('jsx') || fileName.endsWith('tsx')) {
+        plugins.push('jsx');
+    }
+    const ast = parse(sourceCode, {
+        sourceType: 'module',
+        plugins,
+    });
+
+    const fileKey = generateLocaleKey(fileName);
+    const currObj = _.get(extractMap, fileKey);
+    if (!currObj) return 0;
+    const keySet = new Set<string>();
+
+    babelTraverse(ast, {
+        Program: {
+            enter(path) {
+                const hasImport = path.node.body.some((node) => {
+                    if (babelTypes.isImportDeclaration(node)) {
+                        return node.specifiers.some(
+                            (spec) =>
+                                babelTypes.isImportDefaultSpecifier(spec) &&
+                                spec.local.name === importVariable,
+                        );
+                    }
+                    return false;
+                });
+                if (!hasImport) {
+                    path.stop();
+                }
+            },
+            exit() {},
+        },
+        MemberExpression(path) {
+            let node = path.node;
+            const identifiers = [];
+            while (babelTypes.isMemberExpression(node)) {
+                identifiers.unshift(
+                    (node.property as babelTypes.Identifier).name,
+                );
+                node = node.object as babelTypes.MemberExpression;
+            }
+            if (babelTypes.isIdentifier(node)) {
+                identifiers.unshift((node as babelTypes.Identifier).name);
+            }
+            if (
+                identifiers.at(0) === importVariable &&
+                !(
+                    identifiers.at(1) === 'get' &&
+                    babelTypes.isCallExpression(path.parentPath.node)
+                )
+            ) {
+                keySet.add(identifiers[identifiers.length - 1]);
+                path.skip();
+            }
+        },
+    });
+
+    const newObj: Record<string, any> = currObj;
+    Object.keys(currObj).forEach((key) => {
+        if (!keySet.has(key)) {
+            delete newObj[key];
+        }
+    });
+    _.set(extractMap, fileKey, newObj);
+
+    const amount = Object.keys(currObj).length - Object.keys(newObj).length;
+    return amount;
+};
+
+const extractI18nByFileType = (
+    fileName: string,
+    extractMap: Record<string, any>,
+) => {
+    if (['.js', '.ts', '.jsx', '.tsx'].some((ext) => fileName.endsWith(ext))) {
+        return extractI18nFromScript(fileName, extractMap);
+    }
+    return 0;
+};
+
+const clear = async () => {
+    const allFiles = getFilteredFiles(extractDir, excludeDir, excludeFile);
+    const subDirs = await getSubDirectories(localeDir);
+
+    subDirs.map((lang) => {
+        const filePath = path.join(localeDir, `${lang}/index.${type}`);
+        parseLocaleModule(filePath).then((extractMap) => {
+            const amount = allFiles.reduce((amount, file) => {
+                try {
+                    const curr = extractI18nByFileType(file, extractMap);
+                    return amount + curr;
+                } catch (error: any) {
+                    updateLocaleContent(extractMap, filePath);
+                    throw new Error(
+                        `${filePath} 移除文案失败, ${error.message}`,
+                    );
+                }
+            }, 0);
+            success(`${filePath} 共移除${amount}个文案！`);
+            updateLocaleContent(extractMap, filePath);
+        });
+    });
+};
+
+export default clear;
--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,10 @@ import _ from 'lodash';
 import slash from 'slash2';
 import * as fs from 'fs';
 import * as path from 'path';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+import * as babelTypes from '@babel/types';
+import generate from '@babel/generator';
 
 import {
     DEFAULT_CONFIG,
@@ -163,26 +167,174 @@ export const setLocaleValue = (extractMap = {}, key: string, value: string) => {
 };
 
 /**
+ * 将对象转换为 AST 节点
+ * @param obj 要转换的对象
+ * @returns AST 节点
+ */
+export const objectToAst = (
+    obj: Record<string, any> | string,
+): babelTypes.ObjectExpression => {
+    const data = typeof obj === 'string' ? JSON.parse(obj) : obj;
+    const properties = Object.entries(data).map(([key, value]) => {
+        let valueNode: babelTypes.Expression;
+        if (value && typeof value === 'object') {
+            valueNode = objectToAst(value);
+        } else if (typeof value === 'string') {
+            valueNode = babelTypes.stringLiteral(value);
+        } else {
+            valueNode = babelTypes.valueToNode(value);
+        }
+        return babelTypes.objectProperty(
+            babelTypes.stringLiteral(key),
+            valueNode,
+        );
+    });
+
+    return babelTypes.objectExpression(properties);
+};
+
+/**
  * 创建或更新国际化资源文件
  * @param {string} content - 要写入的国际化内容
  */
-export const updateLocaleFile = (content = '') => {
+export const updateLocaleContent = (
+    content: Record<string, any>,
+    filePath?: string,
+) => {
     const { localeDir, type, sourceLocale } = getProjectConfig();
-    const fileType = type || LOCALE_FILE_TYPES.TS;
-    const targetFilename = path.join(
-        localeDir,
-        `${sourceLocale}/index.${fileType}`,
-    );
+    const targetFilename =
+        filePath || path.join(localeDir, `${sourceLocale}/index.${type}`);
     const directory = path.dirname(targetFilename);
 
     if (!fs.existsSync(directory)) {
         fs.mkdirSync(directory, { recursive: true });
     }
 
-    if ([LOCALE_FILE_TYPES.TS, LOCALE_FILE_TYPES.JS].includes(fileType)) {
-        fs.writeFileSync(targetFilename, `export default ${content}`, 'utf8');
+    if ([LOCALE_FILE_TYPES.TS, LOCALE_FILE_TYPES.JS].includes(type)) {
+        const newAst = objectToAst(content);
+
+        const sourceCode = fs.readFileSync(targetFilename, 'utf-8');
+        const ast = parse(sourceCode, {
+            sourceType: 'module',
+            plugins: type === LOCALE_FILE_TYPES.TS ? ['typescript'] : [],
+        });
+
+        let exportedIdentifier: string | null = null;
+
+        traverse(ast, {
+            ExportDefaultDeclaration(path) {
+                const declaration = path.node.declaration;
+                if (babelTypes.isIdentifier(declaration)) {
+                    exportedIdentifier = declaration.name;
+                }
+                if (babelTypes.isObjectExpression(declaration)) {
+                    path.node.declaration = newAst;
+                }
+                path.stop();
+            },
+        });
+
+        if (exportedIdentifier) {
+            traverse(ast, {
+                VariableDeclarator(path) {
+                    if (
+                        babelTypes.isIdentifier(path.node.id) &&
+                        path.node.id.name === exportedIdentifier
+                    ) {
+                        path.node.init = newAst;
+                        path.stop();
+                    }
+                },
+            });
+        }
+
+        const { code } = generate(ast, {
+            jsescOption: {
+                minimal: true,
+            },
+        });
+        fs.writeFileSync(targetFilename, code, 'utf8');
         return;
     }
 
-    fs.writeFileSync(targetFilename, content, 'utf8');
+    fs.writeFileSync(targetFilename, JSON.stringify(content, null, 4), 'utf8');
+};
+
+/**
+ * 获取语言目录下的所有语言文件夹
+ * @param directoryPath 语言目录路径
+ * @returns 返回语言文件夹数组，如 ['zh-CN', 'en-US']
+ */
+export const getSubDirectories = async (
+    directoryPath: string,
+): Promise<string[]> => {
+    if (!fs.existsSync(directoryPath)) {
+        return Promise.reject(`不存在 ${directoryPath} 文件夹`);
+    }
+    return fs
+        .readdirSync(directoryPath)
+        .filter((name) =>
+            fs.statSync(path.join(directoryPath, name)).isDirectory(),
+        );
+};
+
+/**
+ * 解析语言模块文件并提取导出的对象
+ * @param filePath 语言模块文件路径
+ * @returns Record<string, any> 返回模块中导出的语言键值对对象
+ */
+export const parseLocaleModule = (filePath: string) => {
+    const { type } = getProjectConfig();
+    let exportData: Record<string, any> = {};
+    if (!fs.existsSync(filePath)) {
+        error(`${filePath} 文件不存在`);
+        return Promise.reject();
+    }
+
+    const content = fs.readFileSync(filePath, 'utf-8') ?? '{}';
+    if ([LOCALE_FILE_TYPES.JS, LOCALE_FILE_TYPES.TS].includes(type)) {
+        const code = fs.readFileSync(filePath, 'utf-8');
+        const ast = parse(code, {
+            sourceType: 'module',
+            plugins: ['typescript'],
+        });
+        let exportIdentifier: string = '';
+        traverse(ast, {
+            ExportDefaultDeclaration(path) {
+                const declaration = path.node.declaration;
+                if (babelTypes.isIdentifier(declaration)) {
+                    exportIdentifier = declaration.name;
+                }
+                if (babelTypes.isObjectExpression(declaration)) {
+                    exportData = eval(
+                        `(${code.slice(declaration.start ?? 0, declaration.end ?? 0)})`,
+                    );
+                }
+            },
+        });
+        if (!exportIdentifier && !exportData) {
+            return Promise.reject(`解析${filePath}文件失败`);
+        }
+        traverse(ast, {
+            VariableDeclarator(path) {
+                if (
+                    path.node.id.type === 'Identifier' &&
+                    path.node.id.name === exportIdentifier
+                ) {
+                    if (
+                        path.node.init &&
+                        path.node.init.type === 'ObjectExpression'
+                    ) {
+                        exportData = eval(
+                            `(${code.slice(path.node.init.start ?? 0, path.node.init.end ?? 0)})`,
+                        );
+                    }
+                }
+            },
+        });
+    } else {
+        exportData = JSON.parse(content);
+    }
+
+    return Promise.resolve(exportData);
 };
